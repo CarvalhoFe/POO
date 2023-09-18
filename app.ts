50872d83e2786cad4e05331e4e8c0c0d1a562c27
Felipe de Carvalho Frenhan <89522832+CarvalhoFe@users.noreply.github.com>
2023-09-18 09:21:02 -0300

    Update app 11/09pt2.ts

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
 import { Bike } from "./bike";
-import { Rent } from "./rent";
 import { User } from "./user";
 import crypto from 'crypto';
 import * as bcrypt from 'bcrypt';
@@ -7,14 +6,13 @@ import * as bcrypt from 'bcrypt';
 export class App {
     users: User[] = []
     bikes: Bike[] = []
-    rents: Rent[] = []
 
-    findUser(email: string): User {
+    findUser(email: string): User | undefined {
         return this.users.find(user => user.email === email);
     }
 
     async hashPassword(password: string): Promise<string> {
-        const saltRounds = 10; // Aumente isso para mais segurança, se desejar
+        const saltRounds = 10;
         const hash = await bcrypt.hash(password, saltRounds);
         return hash;
     }
@@ -38,7 +36,7 @@ export class App {
     async authenticateUser(userId: string, password: string): Promise<boolean> {
         const user = this.users.find((u) => u.id === userId);
         if (!user) {
-            return false; // Usuário não encontrado
+            return false;
         }
 
         const passwordMatch = await bcrypt.compare(password, user.password);
@@ -47,10 +45,6 @@ export class App {
 
     listUsers(): User[] {
         return this.users;
-    }
-
-    listRents(): Rent[] {
-        return this.rents;
     }
 
     listBikes(): Bike[] {
@@ -64,16 +58,7 @@ export class App {
         return newId;
     }
 
-    removeUser(email: string): void {
-        const userIndex = this.users.findIndex(user => user.email === email);
-        if (userIndex !== -1) {
-            this.users.splice(userIndex, 1);
-            return;
-        }
-        throw new Error('User does not exist.');
-    }
-
-    rentBike(bikeId: string, userEmail: string, startDate: Date, endDate: Date): void {
+    rentBike(bikeId: string, userEmail: string): void {
         const bike = this.bikes.find(bike => bike.id === bikeId);
         if (!bike) {
             throw new Error('Bike not found.');
@@ -82,25 +67,28 @@ export class App {
         if (!user) {
             throw new Error('User not found.');
         }
-        const bikeRents = this.rents.filter(rent =>
-            rent.bike.id === bikeId && !rent.dateReturned
-        );
-        const newRent = Rent.create(bikeRents, bike, user, startDate, endDate);
-        this.rents.push(newRent);
+
+        if (!bike.available) {
+            throw new Error('Bike is not available for rent.');
+        }
+
+        bike.available = false; // Definir a bicicleta como indisponível para aluguel
     }
 
-    returnBike(bikeId: string, userEmail: string) {
-        const today = new Date();
-        const rent = this.rents.find(rent => 
-            rent.bike.id === bikeId &&
-            rent.user.email === userEmail &&
-            rent.dateReturned === undefined &&
-            rent.dateFrom <= today
-        );
-        if (rent) {
-            rent.dateReturned = today;
-            return;
+    returnBike(bikeId: string, userEmail: string): void {
+        const bike = this.bikes.find(bike => bike.id === bikeId);
+        if (!bike) {
+            throw new Error('Bike not found.');
         }
-        throw new Error('Rent not found.');
+        const user = this.findUser(userEmail);
+        if (!user) {
+            throw new Error('User not found.');
+        }
+
+        if (bike.available) {
+            throw new Error('Bike is already available.');
+        }
+
+        bike.available = true; // Definir a bicicleta como disponível
     }
 }
